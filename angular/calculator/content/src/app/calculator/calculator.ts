857ef7fc590c0f74e513550cbe6861edd4a8e33a
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-13 13:21:52 +0100

    adding new project

---
+++ angular/calculator/content/src/app/calculator/calculator.ts
@@ -0,0 +1,179 @@
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [NgClass],
+  templateUrl: './calculator.html',
+  styleUrl: './calculator.css'
+})
+export class CalculatorComponent {
+  /* CONSTS */
+  ERRORS_SPEED = 60;
+
+  /* VARS */
+  
+  addingParam = false;
+  display: string = '0';
+  expressionMemory: string = '';
+  isError = false;
+  operators = ['+', '-', '/', '*'];
+
+  addParam(param: string) {
+    this.addingParam = true;
+    this.expressionMemory = '';
+    if(this.isError) {
+      this.display = '0';
+      this.isError = false;
+    } else {
+      if (this.display === '0') {
+        if(this.operators.includes(param.trim())) {
+          if(param.trim() === "-") {
+            this.display = param;
+          } else {
+            return;
+          }
+        } else {
+          this.display = param;
+        }
+      } else {
+        if(this.operators.includes(param.trim())) {
+          if(this.display.trim().slice(-1) === param.trim() || this.operators.includes(this.display.trim().slice(-1))) {
+            return;
+          } else {
+            this.display += param;
+          }
+        } else {
+          if(param.trim() === ".") {
+            const displayInArray = this.display.split(/\s[+\-*/]\s/);
+            if(displayInArray.slice(0, -1).length > 0) {
+              if(!displayInArray[displayInArray.length-1].includes('.')) {
+                this.display += param;
+              }
+            } else {
+              if(!this.display.includes('.')) {
+                this.display += param;
+              }
+            }
+          } else {
+            this.display += param;
+          }
+        }
+      }
+      this.isError = false;
+    }
+    setTimeout(() => {
+      this.addingParam = false;
+    }, 150);    
+  }
+
+  eval(param: string = "") {
+    if(param) {
+      this.typeError("ERR: Not aviable currently!", this.ERRORS_SPEED);
+      this.isError = true;
+    } else {
+      try {
+        const result = eval(this.display.trim());
+      
+        if (result === Infinity || result === -Infinity || isNaN(result)) {
+          throw new Error('Division by zero');
+        }
+      
+        let displayResult = result.toString();
+        if(result < 0) displayResult = '- ' + Math.abs(result);
+
+        this.expressionMemory = this.display;
+        this.display = displayResult;
+        this.isError = false;
+      } catch (err: any) {
+        if (err.message.includes('Division by zero')) {
+          this.typeError("Error: División por 0", this.ERRORS_SPEED);
+        } else if (err.message.includes('Unexpected end of expression')) {
+          this.typeError("Error de sintaxis", this.ERRORS_SPEED);
+        } else {
+          this.typeError("Operación inválida", this.ERRORS_SPEED);
+        }
+      
+        this.isError = true;
+      }
+      
+    }
+  }
+
+  clearDisplay() {
+    this.expressionMemory = '';
+    this.display = '0';
+    this.isError = false;
+  }
+
+  delete() {
+    this.expressionMemory = '';
+    if(!this.isError) {
+      this.display = this.display.slice(0, -1);
+      if(this.display == '') {
+        this.display = '0';
+      }
+    } else {
+      this.display = '0';
+      this.isError = false;
+    }
+  }
+
+  applyPercentage() {
+    this.expressionMemory = '';
+    try {
+      const parts = this.display.trim().split(/\s([+\-*/])\s/); 
+      if (parts.length === 1) {
+        const num = parseFloat(parts[0]);
+        if (!isNaN(num)) this.display = (num / 100).toString();
+      } else if (parts.length === 3) {
+        const a = parseFloat(parts[0]);
+        const op = parts[1];
+        const b = parseFloat(parts[2]);
+        let result = 0;
+  
+        switch (op) {
+          case '+':
+            result = a + (a * b / 100);
+            break;
+          case '-':
+            result = a - (a * b / 100);
+            break;
+          case '*':
+            result = a * (b / 100);
+            break;
+          case '/':
+            if (b === 0) throw new Error('División por zero');
+            result = a / (b / 100);
+            break;
+        }
+  
+        if(isNaN(result)) {
+          this.typeError("El resultado no es numérico", this.ERRORS_SPEED);
+          this.isError = true;
+        } else {
+          this.display = result.toString();
+          this.isError = false;
+        }
+      } else {
+        throw new Error('Operación inválida');
+      }
+    } catch (err) {
+      this.typeError("Error en la operación", this.ERRORS_SPEED);
+      
+
+      this.isError = true;
+    }
+  }
+  
+
+
+  typeError(msg: string, speed: number = 75) {
+    this.display = "";
+    for (let i = 0; i < msg.length; i++) {
+      setTimeout(() => {
+        this.display += msg.charAt(i);
+      }, i * speed);
+    }
+  }
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,40 @@ import { defineConfig } from 'astro/config';
 import { idiomas, idiomaPorDefecto } from './src/consts/locales';
 import vercel from '@astrojs/vercel/serverless';
 
+import angular from '@analogjs/astro-angular';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: idiomas,
-        defaultLocale: idiomaPorDefecto,
-        routing: {
-            prefixDefaultLocale: true,
+  i18n: {
+      locales: idiomas,
+      defaultLocale: idiomaPorDefecto,
+      routing: {
+          prefixDefaultLocale: true,
+      },
+  },
+
+  site: 'https://yarosprojects.vercel.app',
+  base: '/',
+
+  redirects: {
+      '/': `/${idiomaPorDefecto}/`,
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: vercel({
+      webAnalytics: {
+          enabled: true,
+      },
+  }),
+
+  integrations: [
+    angular({
+        vite: {
+          inlineStylesExtension: 'scss|sass|less',
         },
-    },
-    site: 'https://yarosprojects.vercel.app',
-    base: '/',
-    redirects: {
-        '/': `/${idiomaPorDefecto}/`,
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
+      }),
+  ],
 });
