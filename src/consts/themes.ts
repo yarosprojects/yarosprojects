b9583e3c1869e4a6c8c83b82c04beed46fb00ef8
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-15 22:40:29 +0100

    Fixing Header and adding dark & light modes

---
+++ src/consts/themes.ts
@@ -0,0 +1,17 @@
+export const THEMES = {
+    light: {
+        id: 'light',
+        label: 'Light',
+        icon: 'icon-[carbon--light]',
+    },
+    dark: {
+        id: 'dark',
+        label: 'Dark',
+        icon: 'icon-[material-symbols--dark-mode-outline-rounded]',
+    },
+    system: {
+        id:'system',
+        label: 'System',
+        icon: 'icon-[ion--md-desktop]',
+    }
+};
